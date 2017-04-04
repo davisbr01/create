15762174c04245a6cb183a856899b36d55f8158f
Davis Rash <davisrash@gmail.com>
2017-04-04 19:59:11 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,7 +1,7 @@
 class Settings():
-  """A class to store all settings for Alien Invasion."""
+    """A class to store all settings for Alien Invasion."""
   
-  def __init__(self):
+    def __init__(self):
     """Initialize the game's settings."""
     # Screen settings
     self.screen_width = 1200
@@ -14,6 +14,6 @@ class Settings():
     # Bullet settings
     self.bullet_speed_factor = 1
     self.bullet_width = 3
-    self.bullet_height = 45
+    self.bullet_height = 15
     self.bullet_color = 60, 60, 60
     self.bullets_allowed = 3
