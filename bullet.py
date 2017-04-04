b7593c9d698c9e2a561a843399f76584961ff51a
Davis Rash <davisrash@gmail.com>
2017-04-04 18:39:30 -0400

    Update bullet.py

--- bullet.py
+++ bullet.py
@@ -1,1 +1,14 @@
+import pygame
+from pygame.sprite import Sprite
 
+class Bullet(Sprite):
+    """A class to manage bullets fired from the ship"""
+  
+    def __init__(self, ai_settings, screen, ship):
+        """Create a bullet object at the ship's current position."""
+        super(Bullet, self).__init__()
+        self.screen = screen
+    
+        # Create a bullet rect at (0, 0) and then set correct position.
+        self.rect = pygame.Rect(0, 0, ai_settings.bullet_width,
+            ai_settings.bullet_height)
