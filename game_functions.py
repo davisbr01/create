bcb243c122f02b141a63cd607e8df2a85792b628
Davis Rash <davisrash@gmail.com>
2017-04-13 10:53:43 -0400

    Update game_functions.py

--- game_functions.py
+++ game_functions.py
@@ -67,6 +67,11 @@ def update_bullets(bullets):
     # Check for any bullets that have hit aliens.
     # If so, get rid of the bullet and the alien.
     collisions = pygame.sprite.groupcollide(bullets, aliens, True, True)
+    
+    if len(aliens) == 0:
+        # Destroy existing bullets and create new fleet.
+        bullets.empty()
+        create_fleet(ai_settings, screen, ship, aliens)
         
 def get_number_aliens_x(ai_settings, alien_width):
     """Determine the number of aliens that fit in a row."""
