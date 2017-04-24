b93a39874809109e18e664bca271bcaf53b211fe
ryanlam003 <ryanlam003@gmail.com>
2017-04-24 11:37:23 -0400

    Update Vigenere Encryption.py

--- Vigenere Encryption.py
+++ Vigenere Encryption.py
@@ -61,7 +61,7 @@ def encrypt(test_word, shift_key):
         return encrypted_word
 
 #method that returns the repeated shift key
-def real_shift_key(test_word,shift_key):
+def real_shift_key(test_word, shift_key):
         num_repeats = int(len(test_word) / len(shift_key)) - 1
         num_remainder = len(test_word) % len(shift_key)
         real_shift_key = shift_key
@@ -72,10 +72,10 @@ def real_shift_key(test_word,shift_key):
 
 #method that button calls so that label can update
 def determinec():
-        text.set(encrypt(get_word(),real_shift_key(helper_method(get_word()),helper_method(get_key()))))
+        text.set(encrypt(helper_method(get_word()), real_shift_key(helper_method(get_word()), helper_method(get_key()))))
 
 button_encrypt = Button(text = "Encrypt!", width = 10, height = 1, command = determinec)
-button_encrypt.place(x=130,y=110)
+button_encrypt.place(x = 130, y = 110)
 
 """Decryption Section"""
 
@@ -91,31 +91,30 @@ box_decrypt_key.place(x = 503, y = 180)
 
 text2 = StringVar()
 label_result_decrypt = Label(textvariable = text2)
-label_result_decrypt.place(x=130,y=240)
+label_result_decrypt.place(x = 130,y = 240)
 
 def get_decrypt_word():
-        decrypt_word = box_decrypt.get(1.0,'end-1c')
+        decrypt_word = box_decrypt.get(1.0, 'end-1c')
         return decrypt_word
 
 def get_decrypt_key():
-        decrypt_key = box_decrypt_key.get(1.0,'end-1c')
+        decrypt_key = box_decrypt_key.get(1.0, 'end-1c')
         return decrypt_key
 
 def decrypt(test_word, shift_key):
         decrypted_word = ""
         
         for index in range(len(test_word)):
-                number_shifted = (  (ord(test_word[index]) - 97) - (ord(shift_key[index]) - 97) ) % 26
+                number_shifted = ((ord(test_word[index]) - 97) - (ord(shift_key[index]) - 97) ) % 26
                 decrypted_char = chr(number_shifted+97)
                 decrypted_word += decrypted_char
                 
         return decrypted_word
 
 def determined():
-        text2.set(decrypt(get_decrypt_word(),real_shift_key(helper_method(get_decrypt_word()),helper_method(get_decrypt_key()))))
+        text2.set(decrypt(helper_method(get_decrypt_word()),real_shift_key(helper_method(get_decrypt_word()),helper_method(get_decrypt_key()))))
 
 button_decrypt = Button(text = "Decrypt!", width = 10, height = 1, command = determined)
 button_decrypt.place(x=130,y=210)
 
 frame.mainloop()
-
